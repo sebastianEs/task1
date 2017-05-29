3ca06e05cf46774920a36a0c9235a4ee46d55a85
sebastian <estradis_91@hotmail.com>
2017-05-29 23:18:47 +0200

    more testing react app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Form from './Form.js';
 
 class App extends Component {
-    
+  constructor(props) {
+      super(props);
+      this.state={
+          name: this.props.name,
+          email: this.props.email
+      }
+  }
   render() {
     return (
       <div className="App">
-       <Form />
+       <Form 
+        
+        />
       </div>
     );
   }
--- src/Form.js
+++ src/Form.js
@@ -1,44 +1,64 @@
 import React, {Component} from 'react';
+import App from './App.js'
 
 class Form extends Component {
     constructor(props) {
         super(props);
         this.state={
-            name: 'MyName',
-            email: 'myemail@email.com'
+            name: '',
+            email: ''
         }
         this.handleChange = this.handleChange.bind(this);
     }
     render() {
        return (
-       <div className="form">
-           <h1>Sign up</h1>
-           <input 
-           placeholder="Name" 
-           name='name'
-           type='name'
-           value={this.state.name} 
-           onChange={this.handleChange} />
-           <br />
-           <input 
-           placeholder="Email" 
-           name='email'
-           type='email'
-           value={this.state.email} 
-           onChange={this.handleChange} />
-       </div>
-        
+       <form className='react-form' onSubmit={this.handleSubmit}>
+  <h1>Say Hi!</h1>
+
+  <fieldset className='form-group'>
+   <App htmlFor='formName' title='Full Name:' />
+
+   <input id='formName' className='form-input' name='name' type='text' required onChange={this.handleChange} value={this.state.name} />
+  </fieldset>
+
+  <fieldset className='form-group'>
+   <App htmlFor='formEmail' title='Email:' />
+
+   <input id='formEmail' className='form-input' name='email' type='email' required onChange={this.handleChange} value={this.state.email} />
+  </fieldset>
+
+  <div className='form-group'>
+   <input id='formButton' className='btn' type='submit' placeholder='Send message' />
+  </div>
+ </form>    
        )
     }
     
-    handleChange(e) {
-        const target = e.target;
-        const name = target.name;
-        const value = target.type === target.value;
-        
-        this.setState({
-            [name]: value
-        })
-    }
+    handleChange = (e) => {
+ let newState = {};
+
+ newState[e.target.name] = e.target.value;
+
+ this.setState(newState);
+};
+
+   handleSubmit = (e) => {
+    e.preventDefault();
+
+    let formData = {
+    formSender: this.state.name,
+    formEmail: this.state.email
+ }
+
+ if (formData.formSender.length < 1 || formData.formEmail.length < 1) {
+  return false;
+ }
+
+ this.setState({
+  firstName: '',
+  lastName: '',
+  email: ''
+ });
+};
 }
 export default Form;
